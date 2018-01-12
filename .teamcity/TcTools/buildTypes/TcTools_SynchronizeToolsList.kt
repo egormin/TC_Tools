c07d98b1ebeff4f413538804effc1e0c57a0ffba
egormin <egormin@TeamCity>
2018-01-12 13:06:03 +0300

    TeamCity change in 'TC Tools' project: VCS roots of 'Synchronize Tools list' build configuration were updated

--- .teamcity/TcTools/buildTypes/TcTools_SynchronizeToolsList.kt
+++ .teamcity/TcTools/buildTypes/TcTools_SynchronizeToolsList.kt
@@ -6,4 +6,9 @@ object TcTools_SynchronizeToolsList : BuildType({
     uuid = "16fd2e58-e892-4e1f-a1cf-72029a86765c"
     id = "TcTools_SynchronizeToolsList"
     name = "Synchronize Tools list"
+
+    vcs {
+        root(TcTools.vcsRoots.TcTools_HttpsGithubComEgorminTcToolsRefsHeadsMaster)
+
+    }
 })
