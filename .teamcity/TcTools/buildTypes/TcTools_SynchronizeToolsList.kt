f0ffb68eea3744cda21b7674bb94ce5643a735e8
egormin <egormin@TeamCity>
2018-01-12 13:07:12 +0300

    TeamCity change in 'TC Tools' project: runners of 'Synchronize Tools list' build configuration were updated

--- .teamcity/TcTools/buildTypes/TcTools_SynchronizeToolsList.kt
+++ .teamcity/TcTools/buildTypes/TcTools_SynchronizeToolsList.kt
@@ -1,6 +1,7 @@
 package TcTools.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2017_2.*
+import jetbrains.buildServer.configs.kotlin.v2017_2.buildSteps.script
 
 object TcTools_SynchronizeToolsList : BuildType({
     uuid = "16fd2e58-e892-4e1f-a1cf-72029a86765c"
@@ -10,5 +11,15 @@ object TcTools_SynchronizeToolsList : BuildType({
     vcs {
         root(TcTools.vcsRoots.TcTools_HttpsGithubComEgorminTcToolsRefsHeadsMaster)
 
+    }
+
+    steps {
+        script {
+            name = "=== Synchronization script running ==="
+            scriptContent = """
+                ls
+                pwd
+            """.trimIndent()
+        }
     }
 })
