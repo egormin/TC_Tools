9f9b99cd12b86484975e604a4cbde1270738f9b8
Your Name <you@example.com>
2018-01-13 16:03:27 +0300

    changed

--- .teamcity/TcTools/buildTypes/TcTools_Test.kt
+++ .teamcity/TcTools/buildTypes/TcTools_Test.kt
@@ -12,7 +12,7 @@ object TcTools_Test : BuildType({
     id = "TcTools_Test"
     name = "Test"
     
-val curr = "xxx"
+val curr = "YYY"
 
     params {
         text("text", curr, label = "text", display = ParameterDisplay.PROMPT, allowEmpty = true)
