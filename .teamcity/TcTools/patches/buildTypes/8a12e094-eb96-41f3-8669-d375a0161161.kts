0fc5a06e24e42f9b8b53cd1aedbca7fa4d116ff2
egormin <egormin@TeamCity>
2018-01-15 10:09:08 +0300

    TeamCity change in 'TC Tools' project: runners of 'Test' build configuration were updated

--- .teamcity/TcTools/patches/buildTypes/8a12e094-eb96-41f3-8669-d375a0161161.kts
+++ .teamcity/TcTools/patches/buildTypes/8a12e094-eb96-41f3-8669-d375a0161161.kts
@@ -38,7 +38,7 @@ changeBuildType("8a12e094-eb96-41f3-8669-d375a0161161") {
                 git clone git@github.com:egormin/TC_Tools.git
                 cd TC_Tools 
                 whatToFind=`cat .teamcity/TcTools/buildTypes/TcTools_Test.kt | grep "val curr"`
-                newValue='val curr = "YYY"'
+                newValue='val curr = "HELLO WORLD"'
                 sed -i "s/${'$'}whatToFind/${'$'}newValue/g" .teamcity/TcTools/buildTypes/TcTools_Test.kt
                 echo ${'$'}whatToFind
                 git commit -am "changed"
