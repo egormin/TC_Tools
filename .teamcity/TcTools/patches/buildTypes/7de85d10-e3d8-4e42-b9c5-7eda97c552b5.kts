7f2544b4b1674b6ddb560b453e0097e4ff5c34bc
egormin <egormin@TeamCity>
2018-01-16 11:33:10 +0300

    TeamCity change in 'TC Tools' project: general settings of 'TC_agent_tools_sync' build configuration were updated

--- .teamcity/TcTools/patches/buildTypes/7de85d10-e3d8-4e42-b9c5-7eda97c552b5.kts
+++ .teamcity/TcTools/patches/buildTypes/7de85d10-e3d8-4e42-b9c5-7eda97c552b5.kts
@@ -13,7 +13,7 @@ in the project with uuid = '50967372-bc45-4f8c-908e-44012f72ed5b' and delete the
 create("50967372-bc45-4f8c-908e-44012f72ed5b", BuildType({
     uuid = "7de85d10-e3d8-4e42-b9c5-7eda97c552b5"
     id = "TcTools_Build"
-    name = "Build"
+    name = "TC_agent_tools_sync"
 
     vcs {
         root("TcTools_HttpsGithubComEgorminTcAgentToolsSyncRefsHeadsMaster")
